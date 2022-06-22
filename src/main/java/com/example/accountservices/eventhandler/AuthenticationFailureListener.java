d16d09eda217ea5c09ac2a2fdd20537f02a5e922
TannerLankford1223 <T.Lankford730@gmail.com>
2022-06-22 00:51:26 -0500

    Refactor: user Optionals to handle null

--- src/main/java/com/example/accountservices/eventhandler/AuthenticationFailureListener.java
+++ src/main/java/com/example/accountservices/eventhandler/AuthenticationFailureListener.java
@@ -5,13 +5,14 @@ import com.example.accountservices.entity.Employee;
 import com.example.accountservices.persistence.UserRepository;
 import com.example.accountservices.service.EmployeeAccountService;
 import com.example.accountservices.service.LoggerService;
-import com.example.accountservices.util.AdminOperation;
 import com.example.accountservices.util.LogEvent;
 import com.example.accountservices.util.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
@@ -40,11 +41,18 @@ public class AuthenticationFailureListener implements ApplicationListener<Authen
 
         loggerService.log(LogEvent.LOGIN_FAILED, username, path, path);
 
-        Employee user = userRepo.findByUsernameIgnoreCase(username);
-        if (user != null && !user.hasRole(UserRole.ADMINISTRATOR)) {
+        Optional<Employee> userOpt = userRepo.findByUsernameIgnoreCase(username);
+        Employee user;
+        if (userOpt.isPresent()) {
+            user = userOpt.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+
+        if (!userOpt.get().hasRole(UserRole.ADMINISTRATOR)) {
             int loginAttempt = user.getFailedAttempt() + 1;
             if (loginAttempt >= EmployeeAccountService.MAX_FAILED_ATTEMPTS) {
-                AdminRequest AdminRequest = new AdminRequest(username, AdminOperation.LOCK);
+                AdminRequest AdminRequest = new AdminRequest(username, "LOCK");
                 employeeAccountService.changeAccess(AdminRequest);
                 loggerService.log(LogEvent.BRUTE_FORCE, username, path, path);
                 loggerService.log(LogEvent.LOCK_USER, username, "Lock user " + username, "/api/admin/user/access");
