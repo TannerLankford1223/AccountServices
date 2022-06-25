dbb7a96b87a433223591cef40577f48ba3b0020b
TannerLankford1223 <T.Lankford730@gmail.com>
2022-06-25 14:40:46 -0500

    Implement sql scripts for H2 testing DB

---
+++ src/test/resources/insertUsers-h2.sql
@@ -0,0 +1,3 @@
+INSERT INTO users VALUES  (2, true, 0, 'Doe', null, 'John', 'testpassword', 'john@acme.com');
+INSERT INTO users VALUES  (3, true, 0, 'Doe', null, 'Jane', 'thisIsATestPassword1234', 'jane@acme.com');
+INSERT INTO users VALUES  (4, true, 0, 'McCartney', null, 'Paul', 'BeatlesPassword', 'paul@acme.com');
